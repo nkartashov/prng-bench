f035206bb29dd41b4111ec673685e9e9efdd4bcc
Nikita Kartashov <snailandmail@gmail.com>
2015-07-06 03:13:40 +0300

    Add PCGRandom for comparison
    Added PCGRandom for comparison, using the pure version of the generator.

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ import System.Random (RandomGen)
 import Criterion.Main
 
 import System.Random.TF.Init
+import System.Random.PCG.Fast.Pure
 
 import SplitMix
 
@@ -13,8 +14,13 @@ import PRNGBench
 annotatedGenToIOBenchmark :: RandomGen g => String -> IO g -> IO Benchmark
 annotatedGenToIOBenchmark n gen = gen >>= \g -> return $ genToBenchGroup n g
 
+join :: Monad m => m (m a) -> m a
+join v = v >>= id
+
 benchsIO :: IO [Benchmark]
-benchsIO = sequence [annotatedGenToIOBenchmark "SplitMix" newSplitMix64, annotatedGenToIOBenchmark "TFRandom" newTFGen]
+benchsIO = sequence [annotatedGenToIOBenchmark "SplitMix" newSplitMix64,
+                     annotatedGenToIOBenchmark "TFRandom" newTFGen,
+                     annotatedGenToIOBenchmark "PCGRandom" $ join $ fmap save create]
 
 main :: IO ()
 main = benchsIO >>= defaultMain
